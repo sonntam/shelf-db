f767980bae88f8c6060619c611470c406a194592
Marcus Sonntag <sonntag.marcus@yahoo.com>
2017-08-06 11:59:21 +0200

    cleanup

--- classes/config.php
+++ classes/config.php
@@ -1,37 +1,6 @@
-<?PHP
-# @Date:   2011-05-15T13:51:00+02:00
-# @Last modified time: 2017-03-26T20:53:45+02:00
-
-
-
-/*
-	part-db version 0.1
-	Copyright (C) 2005 Christoph Lechner
-	http://www.cl-projects.de/
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-
-	$Id$
-
-	ChangeLog
-	02/12/2009
-		Split of from lib.php
-*/
+<?php
 
 namespace {
-use PMA\libraries\properties\options\items\BoolPropertyItem;
 
 	require_once(__DIR__.'/config.defaults.php');
 	require_once(__DIR__.'/log.php');
