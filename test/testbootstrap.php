4a1e04346e585b512dd8290f389e1be3396b7c3c
Marcus Sonntag <sonntag.marcus@yahoo.com>
2017-10-21 23:27:01 +0200

    Controller for testbootstrap page

---
+++ test/testbootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+  // This is the initial main controller
+
+  include_once(dirname(__DIR__).'/lib/autoloader.php');
+
+  \Log::Info("Request Uri ".$_SERVER['REQUEST_URI']);
+  \Log::Info("Root ".$_SERVER['DOCUMENT_ROOT']);
+
+  $pdb = ShelfDB::Instance();
+
+  echo $pdb->RenderTemplate('testbootstrap.twig', array(
+    'version'          => array(
+        'programVersion'   => $pdb->GetProgramVersion(),
+        'databaseVersion'  => $pdb->GetDatabaseVersion()
+    ),
+    'logContent'       => Log::FetchLogContent(),
+    'user'             => array(
+      'isLoggedIn'         => $pdb->User()->IsLoggedIn(),
+      'isAdmin'            => $pdb->User()->IsAdmin()
+    )
+  ));
+?>
