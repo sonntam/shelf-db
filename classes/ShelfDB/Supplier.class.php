a0c4ef441d0fdd609b0dc00c2b9a8933211624b0
Marcus Sonntag <marcus.sonntag@planetultra.de>
2018-11-03 15:58:16 +0100

    Create full path to supplier image in GetById routine

--- classes/ShelfDB/Supplier.class.php
+++ classes/ShelfDB/Supplier.class.php
@@ -235,6 +235,11 @@ namespace ShelfDB {
       $children = $res->fetch_all(MYSQLI_ASSOC);
       $res->free();
 
+      foreach( $children as &$child ) {
+        // Create full image path
+        $child['imgPath'] = joinPaths( $this->db()->RelRoot(), 'img/supplier', $child['pict_fname']);
+      }
+
       if( sizeof($children) == 1 ) {
         $children = $children[0];
       }
